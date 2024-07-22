9d5090d59019973af697d546186b84358bdc6dff
Jake Sturgill Hood <jakechood@gmail.com>
2024-07-22 15:58:33 -0500

    Routes map to /home location

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WorldMapComponent } from './world-map/world-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: WorldMapComponent
+    }
+];
