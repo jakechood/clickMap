d1b1a0c1db867dab3162ccd86fe74d232b25479f
Jake Sturgill Hood <jakechood@gmail.com>
2024-07-23 11:56:46 -0500

    Transitions script from separate js file to main ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,3 +20,61 @@ export class AppComponent {
     })
   }
 }
+
+
+//Access SVG object and its content document
+window.addEventListener("load", function() {
+  // Declare map object as variable
+  var svg = document.getElementById("worldMap");
+  // Declare map content as object
+  var svgObject = document.getElementById("worldMap").contentDocument;
+  
+
+})
+$("path").click(function() {
+  let xhr = new XMLHttpRequest();
+  // let requestData = path.id or path.name or path.class
+  $.get("api.geonames.org/citiesJSON?", requestData, function(data) {
+  $.ajax({
+     url: "api.geonames.org/citiesJSON?",
+     type: "GET",
+     data: {
+        geoNameId : "afghanistan",
+        units: "imperial",
+        dataType: 'json',
+        username: "jakechood"
+     },  
+     success : function() {
+        
+     }
+  }).done(function(data) {
+     }).done(function(data) {
+        $.ajax({
+           url: "api.geonames.org/citiesJSON?",
+           type: "GET",
+           data: {
+              geoNameId : "afghanistan",
+              units: "imperial",
+              dataType: 'json',
+              username: "jakechood"
+           },
+     });
+     $("#working").hide();
+     $("#results").show();
+     // Separate featureCode data (bugfix attempt)
+     let countryCapital = data.featureCode(PPLC);
+     let countryRegion = data.featureCode[1];
+     let countryWaterfalls = data.featureCode[2];
+     let countryLakes = data.featureCode[3];
+  // Display the location data retrieved from the API 
+     $("#locName").innerHTML = "Afghanistan";        
+     $("#countryCapital").innerHTML = countryCapital;
+     $("#countryRegion").innerHTML = countryRegion;
+     $("#countryWaterfalls").innerHTML = countryWaterfalls;
+     $("#countryLakes").innerHTML = countryLakes;
+     $(locName).prependTo("#locNameTitle");
+  }).fail(function() {
+  $("#working").hide();
+  $("#error").html("Error displaying location information.");
+  });
+});
