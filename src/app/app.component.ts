865d8269da81e7f5980d654768c83f96d66efc58
Jake Sturgill Hood <jakechood@gmail.com>
2024-07-23 12:43:36 -0500

    Prepares event function for upcoming meeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,37 +15,38 @@ export class AppComponent {
   title = 'Clickable World Map';
   constructor(private countryService: CountryLookupService) {}
   getCountryDetails(event: any) {
-    this.countryService.getCountryData(/*event info*/).subscribe(data => {
-      $("locName").load("./world-map/world-map.component.html");
+    this.countryService.getCountryData($("locName").load("./world-map/world-map.component.html");
 
-      $("path").click(function() {
-        let clicked = EventTarget;
-        // Load location name from world map component
-        $("./demographic-info/demographic-info.component.html #locName").load("./world-map/world-map.component.html");
-        // TODO: How do I fix this?
-        if (clicked.class !== null) {
-          let locName = document.querySelectorAll("path class");
+    $("path").click(function() {
+      let clicked = EventTarget;
+      // Load location name from world map component
+      $("./demographic-info/demographic-info.component.html #locName").load("./world-map/world-map.component.html");
+
+      // TODO: How do I fix this?
+      if (clicked.class !== null) {
+        var locName = document.querySelectorAll("path class");
+      }
+
+      // Initiate new XML Http Request
+      let xhr = new XMLHttpRequest();
+      // TODO: Finish building request here
+
+      // let requestData = path.id or path.name or path.class
+      $.get("api.geonames.org/citiesJSON?", requestData, function(data) {
+      $.ajax({
+        url: "api.geonames.org/citiesJSON?",
+        type: "GET",
+        data: {
+            geoNameId : "afghanistan",
+            units: "imperial",
+            dataType: 'json',
+            username: "jakechood"
+        },  
+        success : function() {
+            
         }
-
-        // Initiate new XML Http Request
-        let xhr = new XMLHttpRequest();
-        
-
-        // let requestData = path.id or path.name or path.class
-        $.get("api.geonames.org/citiesJSON?", requestData, function(data) {
-        $.ajax({
-          url: "api.geonames.org/citiesJSON?",
-          type: "GET",
-          data: {
-              geoNameId : "afghanistan",
-              units: "imperial",
-              dataType: 'json',
-              username: "jakechood"
-          },  
-          success : function() {
-              
-          }
-        }).done(function(data) {
+      });
+    }).subscribe(data => {
           }).done(function(data) {
               $.ajax({
                 url: "api.geonames.org/citiesJSON?",
@@ -59,19 +60,19 @@ export class AppComponent {
           });
           $("#working").hide();
           $("#results").show();
-          
+
         // Separate featureCode data (bugfix attempt)
         let countryCapital = data.featureCode(PPLC);
         let countryRegion = data.featureCode[1];
         let countryWaterfalls = data.featureCode[2];
         let countryLakes = data.featureCode[3];
       // Display the location data retrieved from the API 
-        $("#locName").innerHTML = "Afghanistan";        
-        $("#countryCapital").innerHTML = countryCapital;
-        $("#countryRegion").innerHTML = countryRegion;
-        $("#countryWaterfalls").innerHTML = countryWaterfalls;
-        $("#countryLakes").innerHTML = countryLakes;
-        $(locName).prependTo("#locNameTitle");
+        $("#locName").text = "Afghanistan";        
+        $("#countryCapital").text = countryCapital;
+        $("#countryRegion").text = countryRegion;
+        $("#countryWaterfalls").text = countryWaterfalls;
+        $("#countryLakes").text = countryLakes;
+        $(locName).prependTo("#locNameTitle").text;
       }).fail(function() {
       $("#working").hide();
       $("#error").html("Error displaying location information.");
@@ -79,4 +80,4 @@ export class AppComponent {
     });
         })
       }
-    }};
+    };
