5fbfa24518235d9dba19b629a005d986ebf97cfd
Jake Sturgill Hood <jakechood@gmail.com>
2024-07-26 10:20:05 -0500

    Links components via routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { DemographicInfoComponent } from './demographic-info/demographic-info.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'map',
         component: WorldMapComponent
+    },
+    {
+        path: 'info',
+        component: DemographicInfoComponent
     }
+
 ];
