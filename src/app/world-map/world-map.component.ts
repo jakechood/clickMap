908c001b7560558030f7286f8505186d0eeeff21
Jake Sturgill Hood <jakechood@gmail.com>
2024-07-29 09:15:31 -0500

    Declare array objects on click event

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CountryLookupService } from '../country-lookup.service';
 import { DemographicInfoComponent } from '../demographic-info/demographic-info.component';
 import { ReplaySubject } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-world-map',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './world-map.component.html',
   styleUrl: './world-map.component.css'
 })
@@ -19,14 +20,14 @@ export class WorldMapComponent {
     // Output service call with function targeting mouse target
     @Output(
         this.countryService.getCountryData(event.target.id).subscribe(data => ({
-          console.log(data);
-          // // Separate data into logical variables
-          // locName : data[1][0].name,
-          // countryCapital : data[1][0].capitalCity,
-          // countryRegion : data[1][0].region,
-          // countryLat : data[1][0].latitude,
-          // countryLong : data[1][0].longitude,
-          // countryIncomeLevel : data[1][0].incomeLevel
+          
+          // Separate data into logical variables
+          locName : data[1][0].name,
+          countryCapital : data[1][0].capitalCity,
+          countryRegion : data[1][0].region,
+          countryLat : data[1][0].latitude,
+          countryLong : data[1][0].longitude,
+          countryIncomeLevel : data[1][0].incomeLevel
           })
         )
       );
